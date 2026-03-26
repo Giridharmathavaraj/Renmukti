60b3903c02cdf7a5f329b8bd3ec0e1d8d37286d0
Giridharmathavaraj <giridharmathavaraj@gmail.com>
2026-03-26 18:39:10 +0530

    chore: add Procfile and start script, use process.env.PORT for production

--- src/server.js
+++ src/server.js
@@ -552,6 +552,5 @@ app.use((req, res, next) => {
 });
 
 
-const PORT = 5000;
-
-app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
